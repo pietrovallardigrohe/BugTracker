e1564b53d3929b70ddd887f2da6895a5d5ffed88
Pietro Vallardi Grohe <pietrovallardi29@gmail.com>
2020-12-13 09:02:26 -0300

    login page added

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,6 @@
 function submit() {
 
-    
+    let username = document.getElementById('username').value
+    let password = sjcl.encrypt("password", document.getElementById('password').value)
 
 }
